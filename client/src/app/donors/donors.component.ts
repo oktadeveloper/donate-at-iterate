e1c057187e1cb100d222d9987264473dc8973fe3
Matt Raible <matt.raible@okta.com>
2018-02-23 21:01:28 -0700

    Style and format data, refresh automatically, and make table pretty

--- client/src/app/donors/donors.component.ts
+++ client/src/app/donors/donors.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { DonationService } from '../shared/donation/donation.service';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-donors',
@@ -11,18 +14,34 @@ export class DonorsComponent implements OnInit, OnDestroy {
   sub: Subscription;
   hours: Array<any>;
   dollars: Array<any>;
+  totalHours = 0;
+  totalDollars = 0;
+  timer: Observable<number>;
 
   constructor(private donationService: DonationService) {
+    this.timer = TimerObservable.create(0, 5000);
   }
 
   ngOnInit() {
-    this.sub = this.donationService.getDonors().subscribe(data => {
-      this.hours = data['time'];
-      this.dollars = data['money'];
-    }, error => console.error(error));
+    this.timer.subscribe(() => {
+        this.sub = this.donationService.getDonors()
+          .subscribe(data => {
+            this.hours = data['time'];
+            this.dollars = data['money'];
+            this.totalHours = this.totalDollars = 0;
+            this.hours.forEach(donation => {
+              this.totalHours += Number.parseInt(donation.hours);
+            });
+            this.dollars.forEach(donation => {
+              this.totalDollars += Number.parseInt(donation.amount);
+            });
+          }, error => console.error(error));
+      });
   }
 
   ngOnDestroy() {
-    this.sub.unsubscribe();
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 }
--- client/src/app/thanks/thanks.component.ts
+++ client/src/app/thanks/thanks.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { DonationService } from '../shared/donation/donation.service';
 import { Subscription } from 'rxjs/Subscription';
 import { D3ChartService } from './d3chart.service';
+import { Observable } from 'rxjs/Observable';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 
 @Component({
   selector: 'app-thanks',
@@ -20,26 +22,30 @@ export class ThanksComponent implements OnInit, OnDestroy {
   donationData: any;
   chartOptions: any;
   sub: Subscription;
+  timer: Observable<number>;
 
   constructor(private donationService: DonationService) {
+    this.timer = TimerObservable.create(0, 5000);
   }
 
   ngOnInit() {
-    this.sub = this.donationService.getStats().subscribe((donations: any) => {
-      this.donations = donations;
-      this.chartOptions = {... D3ChartService.getChartConfig()};
-      this.donationData = [
-        {
-          key: 'Hours',
-          y: donations.time,
-          color: '#13C0f9'
-        }, {
-          key: 'Money',
-          y: donations.money,
-          color: '#15F94E'
-        }
-      ];
-    }, error => console.error(error));
+    this.timer.subscribe(() => {
+      this.sub = this.donationService.getStats().subscribe((donations: any) => {
+        this.donations = donations;
+        this.chartOptions = {... D3ChartService.getChartConfig()};
+        this.donationData = [
+          {
+            key: 'Hours',
+            y: donations.time,
+            color: '#13C0f9'
+          }, {
+            key: 'Money',
+            y: donations.money,
+            color: '#15F94E'
+          }
+        ];
+      }, error => console.error(error));
+    });
   }
 
   ngOnDestroy() {
